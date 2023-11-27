0d2e4b040138702b28d31bd29817e9e926a69ee6
kgarcia7 <kgarcia7814@gmail.com>
2023-11-26 21:08:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -52,9 +52,7 @@ let employee_tracker_db = function () {
         employee_tracker_db();
       })
     }
-
     if (answers.prompt === 'Add Department') {
-
       inquirer.prompt([
         {
           type: "input",
@@ -62,21 +60,82 @@ let employee_tracker_db = function () {
           message: "What is the name of the new department?"
         }
       ])
-      .then((answers)=> {
-        
-              db.query("INSERT INTO department (name) VALUES (?)", [answers.name], (err, results) => {
-                if(err) {
-                  console.log(err);
-                  return;
-                }
-                console.log("Added department!")
-                employee_tracker_db();
-              })
-
+    .then((answers)=> {
+      db.query("INSERT INTO department (name) VALUES (?)", [answers.name], (err, results) => {
+        if(err) {
+          console.log(err);
+          return;
+        }
+        console.log("Added department!")
+        employee_tracker_db();
+        })
       })
+     }
 
+    if (answers.prompt === 'Add Role') {
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is the name of the new role?"
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "What is the salary for this role?"
+      }, 
+      {
+        type: "input", 
+        name: "deparment_id",
+        message: "Which department does this role belong to?"
+      }
+      ])
+    .then((answers)=> {
+      db.query("INSERT INTO role (title, salary, department_id) VALUES (?)", [answers.tile, answers.salary, answers.department_id], (err, results) => {
+        if(err) {
+          console.log(err);
+          return;
+        }
+        console.log("Added role!")
+        employee_tracker_db();
+        })
+      })
     }
-  })
+
+    if (answers.prompt === 'Add Employee') {
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "first_name",
+          message: "What is the employee's first name?"
+        },
+        {
+          type: "input",
+          name: "last_name",
+          message: "What is the employee's last name?"
+        },
+        {
+          type: "input",
+          name: "role_id", 
+          message: "What is the role ID?",
+        },
+        {
+          type: "input", 
+          name: "manager_id", 
+          message: "What is the manager ID?"
+        }
+        ])
+      .then((answers)=> {
+        db.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?)", [answers.first_name, answers.last_name, answers.role_id, answers.manager_id], (err, results) => {
+          if(err) {
+            console.log(err);
+            return;
+          }
+          console.log("Added role!")
+          employee_tracker_db();
+          })
+        })
+      }
 }
 
 employee_tracker_db();
